bb434b92c0602361899f58b68f1c78e262e48493
MAMMADASHAHEED <Mshaheed840@gmail.com>
2025-03-15 10:30:12 +0000

    first commit

---
+++ app.py
@@ -0,0 +1,24 @@
+from flask import Flask, render_template, request
+import pickle
+
+app = Flask(__name__)
+
+with open("models/cv.pkl", "rb") as file:
+    cv = pickle.load(file)
+clf = pickle.load(open("models/clf.pkl", "rb"))
+
+@app.route("/")
+def home():
+    return render_template("index.html")
+
+@app.route("/predict", methods=["POST"])
+def predict():
+    email = request.form.get('email-content')  # Corrected field name
+    tokenized_email = cv.transform([email])  # Corrected variable name
+    prediction = clf.predict(tokenized_email)[0]  # Ensure prediction is extracted properly
+    prediction = "Spam" if prediction == 1 else "Not Spam"
+    
+    return render_template("index.html", predictions=prediction, text=email)
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5001, debug=True)  # Changed port to avoid conflict
